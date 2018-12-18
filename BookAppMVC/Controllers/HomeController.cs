43a7f849c22c658443ee87cfa00fdada9e4c93d4
JTu8 <32706871+JTu8@users.noreply.github.com>
2018-12-18 14:35:54 +0200

    Get toimii

---
+++ BookAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookAppMVC.Models;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace BookAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<Books> _books = null;
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50644/api/Books");
+                var responseTask = client.GetAsync("books");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Books>>();
+                    _books = readTask.Result;
+                }
+                else
+                {
+                    _books = Enumerable.Empty<Books>();
+                    ModelState.AddModelError(string.Empty, "Virhe tietokannassa");
+                }
+            }
+
+            return View(_books);
+        }
+
+        
+    }
+}
---
+++ BookAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
