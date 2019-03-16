753ea7d592f5726ed601121c6c04953a232f6773
JTu8 <32706871+JTu8@users.noreply.github.com>
2019-03-16 14:56:38 +0200

    Lisäys toimii päivämäärää lukuunottamatta

--- BookAppMVC/Controllers/HomeController.cs
+++ BookAppMVC/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace BookAppMVC.Controllers
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:50645/api/Books");
+                client.BaseAddress = new Uri("http://localhost:50644/api/Books");
                 var responseTask = client.GetAsync("books");
                 responseTask.Wait();
 
@@ -38,29 +38,32 @@ namespace BookAppMVC.Controllers
             return View(_books);
         }
 
-        [HttpPost]
-        public IActionResult AddBooks(Books books)
+        
+        public ActionResult AddNewBook()
         {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult AddNewBook(Books books)
+        {
             using(var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:50645/api/Books");
+                client.BaseAddress = new Uri("http://localhost:50644/api/Books");
 
                 var postTask = client.PostAsJsonAsync<Books>("books", books);
                 postTask.Wait();
 
                 var result = postTask.Result;
-                if (result.IsSuccessStatusCode)
+                if(result.IsSuccessStatusCode)
                 {
                     return RedirectToAction("Index");
                 }
-
             }
 
             ModelState.AddModelError(string.Empty, "Virhe palvelimessa");
 
-            return View();
-
+            return View(books);
         }
 
     }
