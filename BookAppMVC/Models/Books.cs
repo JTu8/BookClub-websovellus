5396d8d3adce9f79fb0ba9a0b96e22a75217d34c
JTu8 <32706871+JTu8@users.noreply.github.com>
2019-04-12 07:59:03 +0300

    Lisäys päivämäärän kanssa toimii

--- BookAppMVC/Models/Books.cs
+++ BookAppMVC/Models/Books.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,13 @@ namespace BookAppMVC.Models
         public int ID { get; set; }
         public string Kirja_nimi { get; set; }
         public string Kirjailija_nimi { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public string Lainauspvm { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public string Ostospvm { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public string Lukemispvm { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public string Palautuspvm { get; set; }
     }
 }
